41dfb5da82d36219cc0ddfaece8812df4426f261
Kristian Saksvik Munkvold <kristian@lyxen.no>
2014-10-27 19:40:43 +0100

    Ready for the snowshoe game. Good luck.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-  port = 1337,
+  port = 8080,
   app = express(),
   path = require('path');
 
@@ -12,17 +12,17 @@ app.get('/', function (req, res) {
   res.redirect('index.html');
 });
 
-app.get(/\.html$/, function (req, res) {
-  var filepath = req.url.slice(1, req.url.length),
-    jaded = filepath.slice(0, filepath.lastIndexOf('.')) + '.jade';
-
-  console.log(filepath);
-  /* Set LOCAL to false if online .*/
-  res.render(jaded, {
-    pretty: true,
-    local : true
-  });
-});
+//app.get(/\.html$/, function (req, res) {
+//  var filepath = req.url.slice(1, req.url.length),
+//    jaded = filepath.slice(0, filepath.lastIndexOf('.')) + '.jade';
+//
+//  console.log(filepath);
+//  /* Set LOCAL to false if online .*/
+//  res.render(jaded, {
+//    pretty: true,
+//    local : true
+//  });
+//});
 
 console.log("Listening on port: ", port)
 app.listen(port);
