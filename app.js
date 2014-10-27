a1c7be57b4689977a6f72107a92e9700d520ac9b
Carl Martin Rosenberg <github@cmrosenberg.com>
2014-10-27 21:01:47 +0100

    Adding loggin to game:start

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+
+    socket.on('game:start', function (data, ack){
+        console.log(data);
+        console.log(ack);
+    });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
